5c1e26b6b2e7ce591fa7ded88ff1c7a0e8038eb8
ajtowf <ajden.towfeek@gmail.com>
2016-10-11 22:45:36 +0200

    Added localization from controller using IStringLocalizer, same problem.

--- src/LocalizationBug/Controllers/HomeController.cs
+++ src/LocalizationBug/Controllers/HomeController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
 
 namespace LocalizationBug.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IStringLocalizer<HomeController> _localizer;
+
+        public HomeController(IStringLocalizer<HomeController> localizer)
+        {
+            _localizer = localizer;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var localizedString = _localizer["Välkommen"];
+            return View("Index", (string)localizedString);
         }
     }
 }
